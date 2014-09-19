ee4279062ccf21e49dc4df77742e2b6360a47425
Kenta Takagaki <kentaroh.takagaki@gmail.com>
2014-09-19 11:55:34 +0200

    140919

--- NounouW/Data.m
+++ NounouW/Data.m
@@ -16,10 +16,36 @@ NNLoad::usage="Load data objects from files.";
 
 
 (* ::Subsection:: *)
+(*FILTER RELATED: NNFilterData, NNDownsampleData*)
+
+
+NNFilterData::usage="Applies FIR filter to data object and gives resulting filter object.";
+NNDownsampleData::usage="Applies Downsample filter to data object and gives resulting filter object.";
+
+
+(* ::Subsection::Closed:: *)
 (*NNToList*)
 
 
 NNToList::usage="Import data objects into Mathematica List.";
+
+
+(* ::Subsection::Closed:: *)
+(*EVENT RELATED: NNEventSegmentTimestamps, NNEventSelect*)
+
+
+NNEventSegmentTimestamps::usage="Extracts segment timestamps from event record.";
+
+
+NNEventSelect::usage="";
+
+
+(* ::Subsection:: *)
+(*TRACE PLOTTING: NNERPExtract, NNERPPlot*)
+
+
+NNERPExtractTS::usage="Extracts ERP traces from a data object.";
+NNERPPlotTS::usage="Extracts segments from a data object and plots ERP.";
 
 
 (* ::Section:: *)
@@ -29,7 +55,7 @@ NNToList::usage="Import data objects into Mathematica List.";
 Begin["`Private`"];
 
 
-(* ::Subsection:: *)
+(* ::Subsection::Closed:: *)
 (*NNLoad*)
 
 
@@ -41,6 +67,24 @@ NNLoad[args___]:=Message[NNLoad::invalidArgs, {args}];
 
 
 (* ::Subsection:: *)
+(*FILTER RELATED: NNFilterData*)
+
+
+NNFilterData[dataObj_/;HHJavaObjectQ[dataObj,"nounou.data.XData"]]:= 
+	JavaNew["nounou.data.filters.XDataFilterFIR", dataObj];
+
+
+NNFilterData[args___]:=Message[NNFilterData::invalidArgs, {args}];
+
+
+NNDownsampleData[dataObj_/;HHJavaObjectQ[dataObj,"nounou.data.XData"], factor_:10]:= 
+	JavaNew["nounou.data.filters.XDataFilterDownsample", dataObj, factor];
+
+
+NNDownsampleData[args___]:=Message[NNDownsampleData::invalidArgs, {args}];
+
+
+(* ::Subsection::Closed:: *)
 (*NNToList*)
 
 
@@ -57,6 +101,68 @@ NNToList[args___]:=Message[NNToList::invalidArgs, {args}];
 
 
 NNToList[args___]:=Message[NNLoad::invalidArgs, {args}];
+
+
+(* ::Subsection::Closed:: *)
+(*EVENT RELATED: NNEventSegmentTimestamps, NNEventSelect*)
+
+
+NNEventSegmentTimestamps[events_List]:=
+NNEventSegmentTimestamps[events]=
+Module[{tempEventStarts, tempEventEnds, tempSegs},
+	tempEventStarts=Select[events, (#[[1]]==0 && #[[5]]=="Starting Recording")& ];
+	tempEventEnds=Select[events, (#[[1]]==0 && #[[5]]=="Ending Recording")& ];
+	tempSegs={#[[2]], NNEventSegmentEndTSSelect[#, tempEventEnds]}& /@ tempEventStarts;
+	tempSegs=Table[ 
+		If[ n >= Length[tempSegs], tempSegs[[n]], {tempSegs[[n, 1]], Min[tempSegs[[n + 1, 1]],tempSegs[[n, 2]]]} ],
+		{n, 1, Length[tempSegs]}
+	];
+	tempSegs
+];
+
+NNEventSegmentEndTSSelect[startEvent_, eventEnds_]:=
+	SelectFirst[eventEnds, (#[[2]] > startEvent[[2]])&, {Null,Infinity}][[2]];
+
+
+NNEventSegmentTimestamps[args___]:=Message[NNEventSegmentTimestamps::invalidArgs, {args}];
+
+
+NNEventSelect[events_List, eventNo_Integer]:=
+Module[{tempEST},
+	tempEST = NNEventSegmentTimestamps[events];
+	If[eventNo<=0 || Length[tempEST]< eventNo,
+		Message[NNEventSelect::invalidEventNo,Length[events], eventNo];,
+		Select[events, (tempEST[[eventNo, 1]] <= #[[2]] && #[[2]] < tempEST[[eventNo, 2]])&]
+	]
+];
+NNEventSelect::invalidEventNo="The event list only has `1` detected segments, and eventNo `2` is therefore invalid.";
+
+
+NNEventSelect[args___]:=Message[NNEventSelect::invalidArgs, {args}];
+
+
+(* ::Subsection:: *)
+(*TRACE PLOTTING: NNERPExtract, NNERPPlot*)
+
+
+NNERPExtractTS[dataObj_/;HHJavaObjectQ[dataObj,"nounou.data.XData"], channel_Integer, timeStamps_List, {preTs_, postTs_} ]:=
+Module[{tempEvents},
+	tempEvents = JavaNew["nounou.data.ranges.RangeTs", #-preTs, #+postTs]& /@ timeStamps;
+	(dataObj@readTraceAbsA[channel, #]&) /@ tempEvents
+];
+
+
+NNERPExtractTS[args___]:=Message[NNERPExtractTS::invalidArgs, {args}];
+
+
+NNERPPlotTS[dataObj_/;HHJavaObjectQ[dataObj,"nounou.data.XData"], channel_Integer, timeStamps_List, {preTs_, postTs_} ]:=
+Module[{tempERP},
+	tempERP =  NNERPExtractTS[dataObj, channel, timeStamps, {preTs, postTs}];
+	ListLinePlot[tempERP]
+];
+
+
+NNERPPlotTS[args___]:=Message[NNERPPlotTS::invalidArgs, {args}];
 
 
 (* ::Section:: *)
